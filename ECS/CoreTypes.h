86eaf86cbc514ce0f88b4148dfcbc67aed4b7722
remus.jones <remus.jones@hotmail.com>
2025-07-06 12:08:43 +1000

    added ifndef for MAX_ENTITIES, MAX_COMPONENTS & AUTO_REGISTRATION

--- ECS/CoreTypes.h
+++ ECS/CoreTypes.h
@@ -11,8 +11,20 @@ using ComponentTypeId = uint64_t;
 using EntityHandle = uint64_t;
 
 constexpr EntityHandle INVALID_ENTITY = 0;
-constexpr size_t MAX_ENTITIES = 5000;
 
+/* Defines the amount of entities that can be created */
+#ifndef MAX_ENTITIES
+constexpr size_t MAX_ENTITIES = 5000;
+#endif
+
+/* Defines how many components the ECS can register */
+#ifndef MAX_COMPONENTS
 constexpr ComponentType MAX_COMPONENTS = 32;
+#endif
+
+/* Adds a conditional in the AddComponent which checks if the component container exists, if not it adds it. This will add a branch in a potential hot-path */
+#ifndef AUTO_REGISTRATION
+constexpr bool AUTO_REGISTRATION = false;
+#endif
 
 using ComponentMask = std::bitset<MAX_COMPONENTS>;
